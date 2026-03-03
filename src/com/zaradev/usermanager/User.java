963b9f753e265f9531f3376804daf33e908c24c2
Zahra GOARANT <zahra.goarant@gmail.com>
2026-03-03 17:59:29 +0000

    :sparkles: feat: initial user domain and service

---
+++ src/com/zaradev/usermanager/User.java
@@ -0,0 +1,26 @@
+package com.zaradev.usermanager;
+
+public class User {
+
+    private String username;
+    private String email;
+    private int age;
+
+    public User(String username, String email, int age) {
+        this.username = username;
+        this.email = email;
+        this.age = age;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
---
+++ src/com/zaradev/usermanager/UserService.java
@@ -0,0 +1,17 @@
+package com.zaradev.usermanager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserService {
+
+    private List<User> users = new ArrayList<>();
+
+    public void addUser(User user) {
+        users.add(user);
+    }
+
+    public List<User> getAllUsers() {
+        return users;
+    }
+}
